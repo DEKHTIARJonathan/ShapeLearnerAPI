16d5cb56fe6f5e1d6dcfa4dff36e760e0dd449d2
Jonathan DEKHTIAR <contact@jonathandekhtiar.eu>
2015-06-09 16:01:52 +0200

    Installation Dependencies Added

---
+++ Needed To Install/scripts/downloadDependencies.ps1
@@ -0,0 +1,38 @@
+function Pause ($Message="Appuyez sur une touche pour quitter...")
+{
+	Write-Host -NoNewLine $Message
+	$null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+	Write-Host ""
+}
+
+function downloadFile($url, $targetFile)
+{ 
+    "Downloading $url" 
+    $uri = New-Object "System.Uri" "$url" 
+    $request = [System.Net.HttpWebRequest]::Create($uri) 
+    $request.set_Timeout(15000) #15 second timeout 
+    $response = $request.GetResponse() 
+    $totalLength = [System.Math]::Floor($response.get_ContentLength()/1024) 
+    $responseStream = $response.GetResponseStream() 
+    $targetStream = New-Object -TypeName System.IO.FileStream -ArgumentList $targetFile, Create 
+    $buffer = new-object byte[] 10KB 
+    $count = $responseStream.Read($buffer,0,$buffer.length) 
+    $downloadedBytes = $count 
+    while ($count -gt 0) 
+    { 
+        [System.Console]::CursorLeft = 0 
+        [System.Console]::Write("Downloaded {0}K of {1}K", [System.Math]::Floor($downloadedBytes/1024), $totalLength) 
+        $targetStream.Write($buffer, 0, $count) 
+        $count = $responseStream.Read($buffer,0,$buffer.length) 
+        $downloadedBytes = $downloadedBytes + $count 
+    } 
+    "`nFinished Download" 
+    $targetStream.Flush()
+    $targetStream.Close() 
+    $targetStream.Dispose() 
+    $responseStream.Dispose() 
+}
+
+downloadFile "https://3230d63b5fc54e62148e-c95ac804525aac4b6dba79b00b39d1d3.ssl.cf1.rackcdn.com/Anaconda-2.2.0-Windows-x86_64.exe" "./binaries/Anaconda-2.2.0-Windows-x86_64.exe"
+
+Pause
---
+++ Needed To Install/scripts/installAnacondaDependencies.bat
@@ -0,0 +1,6 @@
+"C:\Anaconda\Scripts\anaconda.bat"
+conda update conda -y
+conda install numpy scikit-learn sqlalchemy vtk -y 
+conda install -c https://conda.binstar.org/topper psycopg2-win-py27 -y
+pip install bottle paste
+pause;
