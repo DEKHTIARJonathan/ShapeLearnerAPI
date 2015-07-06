6c9edd9f223b20ac7863a8ca5b97a8222e97ad87
Julien (DeltaCAD) <metis@deltacad.fr>
2015-07-06 17:39:18 +0000

    Installation fixed

--- Needed To Install/scripts/downloadDependencies.ps1
+++ Needed To Install/scripts/downloadDependencies.ps1
@@ -33,7 +33,6 @@ function downloadFile($url, $targetFile)
     $responseStream.Dispose() 
 }
 
-downloadFile "https://3230d63b5fc54e62148e-c95ac804525aac4b6dba79b00b39d1d3.ssl.cf1.rackcdn.com/Anaconda-2.2.0-Windows-x86_64.exe" "../binaries/Anaconda-2.2.0-Windows-x86_64.exe"
 downloadFile "https://3230d63b5fc54e62148e-c95ac804525aac4b6dba79b00b39d1d3.ssl.cf1.rackcdn.com/Anaconda-2.2.0-Windows-x86.exe" "../binaries/Anaconda-2.2.0-Windows-x86.exe"
 
 Pause
